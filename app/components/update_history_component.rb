a7ec024fcf929866f1fa1ed2c7a98c686cf05593
TadesseDev <tadalem26@gmail.com>
2023-03-14 14:35:36 +0300

    Update comment and update histories

--- app/components/update_history_component.rb
+++ app/components/update_history_component.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class UpdateHistoryComponent < ViewComponent::Base
-
+def initialize(update_history=UpdateHistory.new)
+    @update_history = update_history
+  end
 end
